7a067bb36a72c1b4746e5a481aaca07cffa50d20
Andrii Zakharov <khaozavr@gmail.com>
2018-01-10 14:52:28 +0100

    first pass at documentation for tree, split, and eval_split funcs

--- R/split.R
+++ R/split.R
@@ -1,3 +1,16 @@
+#' split
+#'
+#' Helper function to perform a split on a categorical predictor
+#'
+#' @param pred A vector of zeroes and ones
+#' @param out A vector of numeric or intreger values
+#'
+#' @return A matrix with two columns, the first being the values of the outcome
+#' corresponding to the 0 value of the predictor, the second to 1.
+#' @export --DELETE?
+#'
+#' @examples
+#' split1 = split(pred1, out)
 split <- function(pred, out) {
     out_0 = out[pred==0]
     out_1 = out[pred==1]
--- R/tree.R
+++ R/tree.R
@@ -1,3 +1,14 @@
+#' tree
+#'
+#' Builds the decision tree. Uses helper functions "split" and "eval_split" to
+#' perform and evaluate splits.
+#'
+#' @param df
+#'
+#' @return --TBD
+#' @export
+#'
+#' @examples --TBD
 tree <- function(df) {
     n_row = nrow(df)
     n_col = ncol(df)
