95a850e626c166b7e8c259a192fcedf82182033b
andriizakharov <khaozavr@gmail.com>
2018-02-07 16:54:24 +0100

    n_vars is now the num of preds for data, added "response" column

--- R/simdata.R
+++ R/simdata.R
@@ -18,13 +18,13 @@
 #' head(dat)
 simdata <- function(n_obs, n_vars, predictors = "factor", output = "cont") {
     mat = matrix(sample(c(1, 0),
-                        size = n_obs * n_vars,
+                        size = n_obs * (n_vars+2),
                         replace = TRUE),
                  nrow = n_obs,
-                 ncol = n_vars)
-    mat[, ncol(mat)] = abs(rnorm(n_obs, mean = 500, sd = 100))
+                 ncol = n_vars + 2)
+    mat[, ncol(mat)-1] = abs(rnorm(n_obs, mean = 500, sd = 100))
     pred_names = c()
-    for (i in 1:(n_vars-1)) pred_names = c(pred_names, paste0("pred", i))
-    colnames(mat) = c(pred_names, "out")
+    for (i in 1:(n_vars)) pred_names = c(pred_names, paste0("pred", i))
+    colnames(mat) = c(pred_names, "rt", "response")
     return(as.data.frame(mat))
 }
