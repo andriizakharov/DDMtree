dd59e4cf44451fb029467bd2afe7c1e6d06cb533
andriizakharov <khaozavr@gmail.com>
2018-02-27 13:38:04 +0100

    first functioning splitting structure with working plotting!

--- R/split.R
+++ R/split.R
@@ -12,7 +12,7 @@
 #'
 #' @examples
 #' split1 = split(pred1, out)
-split <- function(dat) {
+split <- function(dat, left = 1) {
     # fit model to full data
     f = fit_DDM(getLL_DDM, dat)
 
@@ -30,7 +30,7 @@ split <- function(dat) {
     print(preds)
     # if there are no such candidate predictors left, terminate splitting
     if (is.null(preds)) {
-        tree <- list(params = 0, lr = 0, N = 0, model = f)
+        tree <- list(params = NA, node_id = NA, param_names = "NA", model = f, lr =  NA, df = 1, N = 0)
         tree$caption <- "TERMINAL"
         return(tree)
     }
@@ -64,14 +64,16 @@ split <- function(dat) {
     subset2 = subset(dat, dat[,imax] == 1)
 
     # construct a SEMtree class to store the data
-    tree <- list(params = f$par, lr = results[imax], N = nrow(dat), model = f)
+    tree <- list(node_id = NA, params = NA, param_names = "NA", lr = results[imax], N = nrow(dat), df = 1, model = f)
     class(tree) <- "semtree"
-    tree$caption <- names(dat[imax])
+    if (left == 1) tree$caption <- paste(names(dat[imax]), "== 0")
+    else tree$caption <- paste(names(dat[imax]), "== 1")
 
     # continue splitting recursively
     tree$left_child <- split(subset1)
-    tree$right_child <- split(subset2)
+    tree$right_child <- split(subset2, left = 0)
 
     return(tree)
 }
-# TBD: recording values into a suitable data structure (data.tree?)
+
+# node_id als global variable counter und dann immer +1 für jeden weiteren Node.
